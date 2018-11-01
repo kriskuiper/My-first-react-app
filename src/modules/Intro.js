1066fa7e039879a4119bc5f419e1e48f09c6cda4
kriskuiper <kriskuiper74@gmail.com>
2018-11-01 18:35:49 +0100

    Moduled the standard React page for fun

---
+++ src/modules/Intro.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Intro() {
+    return (
+        <p className="App-intro">
+            Dit is mijn eerste React app.
+        </p>
+    );
+}
+export default Intro;
